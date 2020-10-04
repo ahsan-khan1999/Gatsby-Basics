66e1d1c3f1b20acd6edeb56ac68be581796a8a01
ahsan-khan1999 <ahsan.ak943@gmail.com>
2020-10-04 21:02:48 +0500

    Getting started with Gatsbay

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import About from './About'
+import {Link} from 'gatsby'
+export default function About() {
+    return (
+        <div>
+            <h1>You are At About Page In Gatsby </h1>
+            <Link to='/'>Home</Link>
+            
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
+import About from './About'
+// import Image from "../components/image"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello World of Gatsby</h1>
+      
+      <Link to='/About'>About</Link>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+  )
+
+}
 
 export default IndexPage
